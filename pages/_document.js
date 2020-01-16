72ac5345ba529529a3794012484fbf2a687dd851
SuhaSalahuddin <suha_salahuddin@yahoo.com>
2020-01-16 14:26:10 +0500

    custom document page

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,31 @@
 import Document , {Head, Main, NextScript}  from 'next/document';
 
-class MyDocument extends Document{
+export default class MyDocument extends Document{
   render(){
     return(
-      
+      <html lang='en'>
+        <Head>
+          <meta name='description' content='My portfolio'/>
+          <meta charSet='utf8'/>
+          <meta name='robots' content='no index, no follow'/>
+          <meta name='viewport' content='width= device-width'/>
+
+        <link ref='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'/>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
+        </Head>
+
+        <body>
+          <Main/>
+          <NextScript />
+        </body>
+        <style global jsx>{`
+          body{
+            font-family: 'Roboto', sans-serif;
+          }
+        `} 
+
+        </style>
+      </html>
     )
   }
 }
