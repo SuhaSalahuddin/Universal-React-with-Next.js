e4cb1873f6fa64eae2789a9f221539c4f2d41cc4
SuhaSalahuddin <suha_salahuddin@yahoo.com>
2020-01-16 16:19:43 +0500

    fetching data with getInitialProps

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,41 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import fetch from 'isomorphic-unfetch';
+import { Component } from 'react';
 
-export default () => (
-  <Layout title='About'>
-    <Link href="/">
-      <a>Home</a> 
-    </Link>
-    <br/><br/>
-    <p>A JS programmer</p>
-    <br/><br/>
-    <img src="/static/js-logo.png" alt="JS-logo" height='200px'/>
-  </Layout>
-);
+export default class About extends Component{
+  // state = {
+  //   user: null
+  // };
+
+  static async getInitialProps(){
+    const res = await fetch('https://api.github.com/users/suhasalahuddin');
+    const data = await res.json();
+
+    return {user: data};
+  }
+
+  // componentDidMount(){
+  //   fetch('https://api.github.com/users/suhasalahuddin')
+  //   .then(res => res.json())
+  //   .then(data => {
+  //     this.setState({
+  //       user: data
+  //     });
+  //   })
+  // }
+
+  render() {
+    const {user} = this.props;
+
+    return (
+      <Layout title='About'>
+        
+        <br/><br/>
+        <p>{user.name}</p>
+        <br/><br/>
+        <img src={user.avatar_url} alt="Suha" height='200px'/>
+      </Layout>
+    );
+  }
+}
