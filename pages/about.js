4f54ead7a6ea54cb3ca6a34657f8079751b0b231
SuhaSalahuddin <suha_salahuddin@yahoo.com>
2020-01-16 18:41:33 +0500

    custom error page

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,15 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
+import Error from './_error';
 import { Component } from 'react';
 
 export default class About extends Component{
-  // state = {
-  //   user: null
-  // };
-
   static async getInitialProps(){
     const res = await fetch('https://api.github.com/users/suhasalahuddin');
+    const statusCode = res.status > 200 ? res.status : false;
     const data = await res.json();
 
-    return {user: data};
+    return {user: data, statusCode};
   }
 
   // componentDidMount(){
@@ -26,13 +23,18 @@ export default class About extends Component{
   // }
 
   render() {
-    const {user} = this.props;
+    const {user, statusCode} = this.props;
+
+    if(statusCode){
+      return <Error statusCode={statusCode} />;
+    }
 
     return (
       <Layout title='About'>
         
         <br/><br/>
         <p>{user.name}</p>
+        <p>{user.bio}</p>
         <br/><br/>
         <img src={user.avatar_url} alt="Suha" height='200px'/>
       </Layout>
